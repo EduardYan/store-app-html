2dbfb7ba80e4481bd6f7b0a6d9df7374fb95228a
EduardYan <eduarygp@gmail.com>
2021-12-31 08:35:23 -0600

    upload code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Store App HTML.
+
+__This is a store app maked with html, plain css and a little javascript.__
+
+# Screenshot.
+![screenshot](./doc/screenshot.png)
+
+# Resources.
+* Images from [pexels.com](www.pexels.com)
---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * This is the javascript for the page is very simple.
+ */
+
+const showMessage = () => {
+  alert('Buy');
+};
+
+document.querySelector(".list-products").addEventListener("click", () => {
+  showMessage();
+});
